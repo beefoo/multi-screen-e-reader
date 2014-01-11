23e4712cca922d95de2c472cd8e0db48f6d7da51
clickslide <hello@clickslide.co>
2014-01-11 15:44:48 -0500

    Initial server file

---
+++ server.js
@@ -0,0 +1,45 @@
+var app = require('http').createServer(handler),
+    io = require('socket.io').listen(app),
+    fs = require('fs')
+
+    app.listen(8080);
+
+function handler(req, res) {
+    fs.readFile(__dirname + '/index.html',
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.sockets.on('connection', function (socket) {
+//    socket.emit('news', {
+//        hello: 'world'
+//    });
+    socket.on('start', function (data) {
+        console.log(data);
+    });
+    socket.on('pageTurn', function (data) {
+        console.log(data);
+        socket.emit('data', {
+            pageNumber: data.pageNumber
+        });
+    });
+    socket.on('invokeQuiz', function (data) {
+        console.log(data);
+        socket.emit('data', {
+            pageNumber: data.pageNumber
+        });
+    });
+    socket.on('quizFinished', function (data) {
+        console.log(data);
+        socket.emit('data', {
+            pageNumber: data.pageNumber
+        });
+    });
+});
