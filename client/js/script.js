b65c80af50964dc8f386408ba8d1a017fd016440
clickslide <hello@clickslide.co>
2014-01-12 01:17:43 -0500

    adding images and scripts to form book. This includes working pagination and all styles to look good on Google Chrome

--- client/js/script.js
+++ client/js/script.js
@@ -1,7 +1,18 @@
+
 $(function(){
+    
+    // connect to the socket
     var socket = io.connect('http://localhost:8080');
+    // keep the ID of the Page to display
+    var state = 0;
+    var states = ["cover", "page01", "page02", "page03", "page04", "page05"];
+    var methods = {};
+    
+    // set initial state once socket connects
     socket.on('connect', function(data) {
-        socket.emit("pageChange", {"pageNumber":1});
+        // set CSS on init
+        $(".lastPage").css("visibility", "hidden");
+        methods.changePage(state);
     });
     socket.on('pageChange', function(data) {
         console.log(data);
@@ -12,4 +23,40 @@ $(function(){
     socket.on('quizFinished', function(data) {
         console.log(data);
     });
-});
+    
+    // next and last button clicks
+    $(".nextPage").bind("click", function(ev) {
+        methods.changePage( (state + 1) );
+    });
+    $(".lastPage").bind("click", function(ev) {
+        methods.changePage( (state - 1) );
+    });
+    
+    methods.changePage = function(page){
+        console.log(page);
+        state = page;
+        for(var i=0; i<6; i++){
+            if(i != page){
+                $("#"+states[i]).css("display", "none");
+            }
+        }
+        $("#"+states[page]).css("display", "inherit"); 
+        if(page == 0){
+            $(".lastPage").css("visibility", "hidden");
+            $(".nextPage").css("visibility", "visible");
+        }
+        if(page == 5) {
+            $(".lastPage").css("visibility", "visible");
+            $(".nextPage").css("visibility", "hidden");
+        }
+        if(page < 5 && page > 0) {
+            $(".lastPage").css("visibility", "visible");
+            $(".nextPage").css("visibility", "visible");
+        }
+    } 
+    
+    methods.triggerQuiz = function(page) {
+        // show the quiz based on the page# in a modal   
+    }
+    
+});  
